a4cfe551363085e527499d0c51af477d85de3b88
Adam Royal <singlepigeon@Adams-MacBook-Pro.local>
2023-03-19 20:56:46 +0000

    Update projects page

---
+++ src/projects.ts
@@ -0,0 +1,15 @@
+type Project = {
+  title: string;
+  description: string;
+  href: string;
+};
+
+const projects: Project[] = [
+  {
+    title: "pigeon-type",
+    description: "A fun, speed-typing test. It's still a WIP.",
+    href: "https://pigeon-type.waterbuckit.dev",
+  },
+];
+
+export default projects;
