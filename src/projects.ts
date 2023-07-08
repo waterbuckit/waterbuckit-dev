1a6b5eef9148c90fa62a3555528c25ef677bc497
Adam Royal <singlepigeon@Adams-MacBook-Pro.local>
2023-07-08 19:50:24 +0100

    Update projects page and photo

--- src/projects.ts
+++ src/projects.ts
@@ -6,10 +6,20 @@ type Project = {
 
 const projects: Project[] = [
   {
+    title: "musings",
+    description: "A collection of literary works done with my twin electron.",
+    href: "https://musings.waterbuckit.dev",
+  },
+  {
     title: "pigeon-type",
-    description: "A fun, speed-typing test. It's still a WIP.",
+    description: "A little speed-typing test. It's still a WIP.",
     href: "https://pigeon-type.waterbuckit.dev",
   },
+  {
+    title: "eidam-and-stelleider",
+    description: "just two dooks, loafing",
+    href: "https://eidam-and-stelleider.waterbuckit.dev",
+  }
 ];
 
 export default projects;
